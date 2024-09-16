c9f41e71851c69c09ef40e6f61272e54f4997833
My comp <My comp@DESKTOP-HBDEDEI>
2024-09-16 10:50:54 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using lr1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lr1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Info()
+        {
+            var company = new Company
+            {
+                Name = "Infinity St.",
+                YearFounded = 2005,
+                EmployeeCount = 300,
+            };
+
+            return Json(company);
+        }
+
+        public IActionResult RandomNumber()
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(0, 101);
+            return Json(new { RandomNumber = randomNumber });
+        }
+    }
+}
---
+++ Models/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace lr1.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public int YearFounded { get; set; }
+        public int EmployeeCount { get; set; }
+    }
+}
